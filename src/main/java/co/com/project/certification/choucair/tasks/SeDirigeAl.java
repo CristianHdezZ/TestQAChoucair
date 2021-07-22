da8e0f8712aa87eb70519b2ef83e94b3e4a7a9ac
chernandezz <uh5gdlqj2biqllfp5547y6plpz4qywessnse5f4kfby7rwdc3wyq>
2021-07-22 02:22:51 -0500

    Initial commit

---
+++ src/main/java/co/com/project/certification/choucair/tasks/SeDirigeAl.java
@@ -0,0 +1,25 @@
+package co.com.project.certification.choucair.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Scroll;
+
+import static co.com.project.certification.choucair.userinterface.ModuloEmpleoPage.*;
+
+public class SeDirigeAl implements Task {
+
+    public static SeDirigeAl PortalEmpleo() {
+        return Tasks.instrumented(SeDirigeAl.class);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Click.on(BUTTON_ACEPTAR_COOKIES),
+                Click.on(BUTTON_OPCION_PORTAL_EMPLEO),
+                Click.on(BUTTON_CONTINUAR)
+        );
+    }
+}
